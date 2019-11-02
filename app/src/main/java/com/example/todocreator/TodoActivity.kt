889199eb1709b3b3e7413801b89853e0b6ff4b29
mafub <mattbrooker22@gmail.com>
2019-11-02 14:55:33 +0000

    SwipeRefreshLayout listener + set up activity dependencies/viewmodel/bindings

--- app/src/main/java/com/example/todocreator/TodoActivity.kt
+++ app/src/main/java/com/example/todocreator/TodoActivity.kt
@@ -1,14 +1,25 @@
 package com.example.todocreator
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.example.todocreator.databinding.ActivityTodoBinding
+import com.example.todocreator.viewmodels.TodoViewModel
 import dagger.android.AndroidInjection
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasAndroidInjector
 import javax.inject.Inject
 
-class TodoActivity : AppCompatActivity(), HasAndroidInjector {
+class TodoActivity : AppCompatActivity(), HasAndroidInjector, SwipeRefreshLayout.OnRefreshListener {
+
+    @Inject
+    lateinit var viewModelFactory: ViewModelProvider.Factory
 
     @Inject
     lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Any>
@@ -17,9 +28,34 @@ class TodoActivity : AppCompatActivity(), HasAndroidInjector {
         return dispatchingAndroidInjector
     }
 
+    val context: Context = this
+    lateinit var binding: ActivityTodoBinding
+
+    private lateinit var todoViewModel : TodoViewModel
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_todo)
+
+        binding = DataBindingUtil.setContentView<ActivityTodoBinding>(this, R.layout.activity_todo)
+        setSupportActionBar(binding.toolbar)
+
+        binding.todoSwipeRefresh
+
+        // Initialize the to do view model using injected viewModelFactory.
+        todoViewModel = ViewModelProviders.of(this, viewModelFactory)[TodoViewModel::class.java]
+
+
+    }
+
+    fun setupTodoList() {
+       // binding.todoList.layoutManager = LinearLayoutManager(this)
+        //adapter = RepositoriesAdapter()
+       // binding.todoList.adapter = adapter
+    }
+
+    override fun onRefresh() {
+
     }
 }
