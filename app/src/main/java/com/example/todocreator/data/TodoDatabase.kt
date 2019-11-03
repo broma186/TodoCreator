f0d22e221592c9bdf0c34a09ef7538ac9e3a7ffe
mafub <mattbrooker22@gmail.com>
2019-11-03 14:26:01 +0000

    Pager dependencies

--- app/src/main/java/com/example/todocreator/data/TodoDatabase.kt
+++ app/src/main/java/com/example/todocreator/data/TodoDatabase.kt
@@ -9,21 +9,6 @@ import com.example.todocreator.utils.DATABASE_NAME
 import com.example.todocreator.data.Todo
 import com.example.todocreator.data.TodoDao
 
-/*
-Not needed yet but can be implemented so that each repository is stored in the
-database.
-
-@Database(entities = [FavoriteShow::class], version = 2, exportSchema = false)
-abstract class TvMazeDatabase : RoomDatabase() {
-
-    abstract fun showDao(): ShowDao
-
-    companion object {
-        const val DATABASE_NAME = "tvmaze.db"
-    }
-}
-
- */
 @Database(entities = [Todo::class], version = 1, exportSchema = false)
 abstract class TodoDatabase : RoomDatabase() {
 
